23725f8c08a354abbd04f707a960fbc1cf555b71
unknown <rajsahildumaribhui@gmail.com>
2021-02-27 07:45:47 +0530

    third commit

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// app.use(express.static(_dirname + '/dist'));
+// app.listen(process.env.PORT || 8080);
+
+// //path location strategy
+
+// app.get('/*', function(req, res) {
+//     res.sendFile(path.join(_dirname + '/dist/index.html'));
+// })
+
+// console.log('console listening');
+//Install express server
+
 const express = require('express');
-const app = express();
 const path = require('path');
-app.use(express.static(_dirname + '/dist'));
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/possibillion'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/possibillion/'}),
+);
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-//path location strategy
-
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(_dirname + '/dist/index.html'));
-})
-
-console.log('console listening');
